d9434fecc1de3f301442a111b92b75f6893f96e3
WahajAhmedIqbal <wahaj465@gmail.com>
2020-10-13 20:23:03 +0500

    updating search at 1.14

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
-    <div className="header">
+    <nav className="header">
       <Link to="/login">
         <img
           className="header_logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
       </Link>
-{/* Search box */}
-    
-
-    </div>
+      {/* Search box */}
+      <div className="header_search">
+        <input type="text" className="header_searchinput" />
+        <SearchIcon className="header_searchicon" />
+      </div>
+    </nav>
   );
 }
 
